89841d0e201b110a06934d8db4df0cc230d767eb
kimj27 <kimj27@oregonstate.edu>
2025-08-23 00:47:49 -0700

    Fixed .cjs file

--- ANC/reactServer.cjs
+++ ANC/reactServer.cjs
@@ -1,25 +1,19 @@
 // reactServer.cjs
-// Uses common JavaScript to serve the React build folder (/dist)
-
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// We will simply hardcode the react PORT, but normally this should be inside .env
 const PORT = 3001;
 
-// Serve the static files from the React app located in the build folder '/dist'
-// React router will take over frontend routing
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the React build inside ANC/dist
+app.use(express.static(path.join(__dirname, 'ANC', 'dist')));
 
-// Handles any requests that don't match the ones above to return the React app
-// A request to '/nonExist' will redirect to the index.html where react router takes over at '/'
+// Handle any requests that don't match the above
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ANC', 'dist', 'index.html'));
 });
 
-// Start the server and listen on the specified port
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server running: http://18.117.218.170:${PORT}...`);
 });
-
