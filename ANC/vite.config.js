59ce84006cf65355df1807897b2d8645d323d1e0
kimj27 <kimj27@oregonstate.edu>
2025-08-25 01:45:18 -0700

    vite config edit

--- ANC/vite.config.js
+++ ANC/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, 'ANC'), // frontend folder
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, 'dist'), // output folder for deployment
+    emptyOutDir: true, // clean before build
+  },
 })
